4de1438b389fd3c68807cd78513bd60fa0270ef2
sofia12k <145674708+sofia12k@users.noreply.github.com>
2025-04-08 13:48:05 +0300

    Create categories.controller.js

---
+++ categories.controller.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+  angular.module('MenuApp')
+    .controller('CategoriesController', CategoriesController);
+
+  CategoriesController.$inject = ['items'];
+  function CategoriesController(items) {
+    var $ctrl = this;
+    $ctrl.items = items;
+  }
+})();
